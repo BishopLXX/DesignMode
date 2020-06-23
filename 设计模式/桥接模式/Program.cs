b6ba985509026415157a522032511b6b054d1cec
liuxiaoxiao <1065742155@qq.com>
2020-06-24 02:57:38 +0800

    增加多种设计模式游戏实现

--- 设计模式/桥接模式/Program.cs
+++ 设计模式/桥接模式/Program.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using System.Text;
 
+/*
+ * 桥接模式适用于：当设计多个角色，多个武器，任意角色可以使用任意武器的时候。
+ * 分别定义一个角色接口和武器接口，武器接口定义开火方法，角色接口定义开火方法（调用武器接口的开火方法）。
+ */
+
+
 namespace 桥接模式
 {
     abstract class Implementor
@@ -45,6 +51,14 @@ namespace 桥接模式
             implementor.Operation();
         }
     }
+
+    class RefinedAbstraction2 : Abstraction
+    {
+        public override void Operation()
+        {
+            implementor.Operation();
+        }
+    }
     
 
     class Program
@@ -56,6 +70,11 @@ namespace 桥接模式
             ab.Operation();
             ab.SetImplementor(new ConcreteImplementorB());
             ab.Operation();
+            Abstraction ab2 = new RefinedAbstraction2();
+            ab2.SetImplementor(new ConcreteImplementorA());
+            ab2.Operation();
+            ab2.SetImplementor(new ConcreteImplementorB());
+            ab2.Operation();
             Console.Read();
         }
     }
